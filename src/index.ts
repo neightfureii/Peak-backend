b0d3ba30026d3e4ef758e3f301af669c034c4fd2
Amantha Tharusha <pmatsrikantha@gmail.com>
2026-05-06 08:46:25 +0530

    implemented get all sports with cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express from "express";
+import cors from "cors";
+import sportsRouter from "./routes/sports.ts";
 
 const app = express();
 const PORT = 8000;
 
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}))
+
 // Middleware
 app.use(express.json());
+
+app.use('/api/sports', sportsRouter);
 
 // Root GET route
 app.get("/", (req, res) => {
